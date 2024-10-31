f7edac8ea273f7f41e8bdeca90fef8fd0404388a
xunana <3520653564@qq.com>
2024-10-31 20:27:41 +0800

    feat:添加heading

---
+++ notion/app/(marketing)/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@/components/ui/button';
+import React from 'react'
+
+const MarketingPage=()=> {
+  return (
+    <div className='min-h-full flex flex-col'>MarketingPage
+        <Button>MarketingPage</Button>
+    </div>
+  )
+}
+
+export default MarketingPage;
