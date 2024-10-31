edce17bf45dcc3e45076c804e40a81f52111a33c
xunana <3520653564@qq.com>
2024-10-31 20:28:17 +0800

    feat:添加heading

--- notion/app/(marketing)/page.tsx
+++ notion/app/(marketing)/page.tsx
@@ -1,12 +1,15 @@
 import { Button } from '@/components/ui/button';
 import React from 'react'
-
-const MarketingPage=()=> {
-  return (
-    <div className='min-h-full flex flex-col'>MarketingPage
-        <Button>MarketingPage</Button>
-    </div>
-  )
-}
-
-export default MarketingPage;
+import Heading from "./_components/heading";
+const MarketingPage = () => {
+    return (
+      <div className="min-h-full flex flex-col dark:bg-[#1F1F1F]">
+        <div className="flex flex-col items-center justify-center md:justify-start text-center gap-y-8 flex-1 px-6 pb-10">
+           <Heading />
+          {/* <Heroes />  */}
+        </div>
+        {/* <Footer /> */}
+      </div>
+    );
+  };
+  export default MarketingPage;
